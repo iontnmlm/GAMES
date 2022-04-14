387a8aa3868a088d3d4fab57afeb0d5f9c1d991b
eliRub <103374937+eliRub@users.noreply.github.com>
2022-04-14 13:06:54 +0300

    Update Window.java

--- Game/Window.java
+++ Game/Window.java
@@ -12,6 +12,7 @@ public class Window  extends JFrame {
     public static final int WINDOW_WIDTH = 800;
     public static final int WINDOW_HEIGHT = 600;
 
+    //This constractur build a window.
     public Window() {
         this.setVisible(true);
         this.setSize(WINDOW_WIDTH , WINDOW_HEIGHT);
@@ -19,24 +20,9 @@ public class Window  extends JFrame {
         this.setLocationRelativeTo(null);
         this.setResizable(false);
         this.setLayout(null);
-
+    //This constractur build a Panel on the window.
         GameScene gameScene = new GameScene(0, 0 , WINDOW_WIDTH, WINDOW_HEIGHT);
         this.add(gameScene);
-
-//        this.setTitle("GAME WINDOW");
-//        this.getContentPane().setBackground(new Color(123, 213,234));//Numbers between 0-255
-//        this.getContentPane().setBackground(new Color(0x123456));
-
-//        JLabel jLabel = new JLabel();
-//        jLabel.setText("Hello");
-//        jLabel.setBounds(0,0, 100, 100);
-//        Font font = new Font("Arial", Font.PLAIN, 30);
-//        jLabel.setFont(font);
-//        jLabel.setBackground(Color.gray);
-//        jLabel.setForeground(Color.WHITE);
-//        jLabel.setOpaque(true);
-//        jLabel.setBorder(BorderFactory.createLineBorder(Color.cyan, 3));
-//        this.add(jLabel);
 
 
     }
