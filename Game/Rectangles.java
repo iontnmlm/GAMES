8bdee55c4a1aee3921fe49740e6c9150676b1e1a
eliRub <103374937+eliRub@users.noreply.github.com>
2022-04-17 01:27:57 +0300

    Add files via upload

---
+++ Game/Rectangles.java
@@ -0,0 +1,52 @@
+package Game;
+
+import com.company.CustomRectangle;
+
+import java.awt.*;
+
+public class Rectangles {
+
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private Color color;
+
+    public Rectangles(int x, int y, int width, int height, Color color){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    public void paint(Graphics g){
+        g.setColor(this.color);
+        g.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    public void moveRight(){
+        this.x++;
+    }
+    public void moveLeft(){
+        this.x--;
+    }
+    public void moveUp(){
+        this.y--;
+    }
+    public void moveDown(){
+        this.y++;
+    }
+
+
+    public boolean checkCollision(Rectangles other){
+        boolean collision = false;
+        Rectangle thisRectangle = new Rectangle(this.x, this.y, this.width, this.height);
+        Rectangle otherRectangle = new Rectangle(other.x, other.y, other.width, other.height);
+        if(thisRectangle.intersects(otherRectangle)){
+            collision = true;
+        }
+        return collision;
+    }
+
+}
