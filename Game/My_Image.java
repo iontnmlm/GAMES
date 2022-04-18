c8ac5f24a0d1f9e1b19227e134da701caeccc06e
eliRub <103374937+eliRub@users.noreply.github.com>
2022-04-19 01:18:01 +0300

    Update My_Image.java

--- Game/My_Image.java
+++ Game/My_Image.java
@@ -1,4 +1,4 @@
-package Game;
+ package Game;
 
 import javax.swing.*;
 import java.awt.*;
@@ -8,39 +8,42 @@ public class My_Image extends Component {
     private ImageIcon imageCar;
     private int x;
     private int y;
+    public Frame frame;
 
-    public My_Image(ImageIcon imageCar, int x, int y ){
+    public My_Image(ImageIcon imageCar, int x, int y, Frame frame){
         this.imageCar = imageCar;
         this.x = x;
         this.y = y;
-    }
+        this.frame = frame;
+     }
 
     public void paint(Graphics g){
+        this.frame.paint(g);
         this.imageCar.paintIcon(this, g, this.x, this.y);
     }
 
     public void moveRight(){
-        this.x += 2;
+        this.x += 4;
     }
     public void moveLeft(){
-        this.x -= 2;
+        this.x -= 4;
     }
-    public void moveUp(){
-        this.y -= 3;
-    }
-    public void moveDown(){
-        this.y += 3;
-    }
+
+//    public void moveUp(){
+//        this.y -= 3;
+//    }
+//    public void moveDown(){
+//        this.y += 3;
+//    }
 
     public void moveCarsDown(){
         this.y += 1;
     }
-    
+
     public int getX() {
         return this.x;
     }
     public int getY(){
         return this.y;
     }
-
 }
